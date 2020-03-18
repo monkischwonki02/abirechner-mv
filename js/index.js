5f28d0794f04f7772f8134b231ab8ecafe652553
Jonas <jonas.klugmann@gmail.com>
2020-03-18 23:07:33 +0100

    TOC einblenden, wenn mit Smartphone aufgerufen

--- js/index.js
+++ js/index.js
@@ -3,7 +3,27 @@ new Vue({
     data: {
         umrechnungKlausurNoten : [95,90,85,80,75,70,65,60,55,50,45,40,33,27,20,0],
         umrechnungSonstNoten : [98.67,97.33,96,90.67,85.33,80,73.33,66.67,60,55.33,46.67,40,33.33,26.67,20,0],
-        punktzahl: ""
+        punktzahl: "",
+        mobileAgent: {
+            Android() {
+                return navigator.userAgent.match(/Android/i)
+            },
+            BlackBerry() {
+                return navigator.userAgent.match(/BlackBerry/i);
+            },
+            iOS() {
+                return navigator.userAgent.match(/iPhone|iPod/i);
+            },
+            Opera() {
+                return navigator.userAgent.match(/Opera Mini/i);
+            },
+            Windows() {
+                return navigator.userAgent.match(/IEMobile/i);
+            },
+            any() {
+                return ((this.Android() || this.BlackBerry() || this.iOS() || this.Opera() || this.Windows()));
+            }
+        }
     },
 
     methods: {
@@ -43,11 +63,19 @@ new Vue({
     },
     created(){
         let URLSearcher = new URLSearchParams(window.location.search)
+        let toc = document.querySelector(".toc")
 
         if (URLSearcher.has("punkte")){
             this.punktzahl = URLSearcher.get("punkte")
         } else {
             this.punktzahl = ""
+        }
+
+        // Ueberprueft das Geraet, ob es ein Mobiles Endgeraet ist
+        if (this.mobileAgent.any()){
+            toc.style.display = 'block'
+        } else {
+            toc.style.display = 'none'
         }
     }
 
