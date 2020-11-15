ba54da19fe9f528ab00d155d8b01a1827d53e9cf
Jonas <jonas.klugmann@gmail.com>
2020-11-15 01:53:23 +0100

    add fields for blockI

--- js/index.js
+++ js/index.js
@@ -3,7 +3,17 @@ new Vue({
     data: {
         umrechnungKlausurNoten : [95,90,85,80,75,70,65,60,55,50,45,40,33,27,20,0],
         umrechnungSonstNoten : [98.67,97.33,96,90.67,85.33,80,73.33,66.67,60,55.33,46.67,40,33.33,26.67,20,0],
-        punktzahl: "",
+        semester= [leistungskurs0, leistungskurs1, leistungskurs2, leistungskurs3, leistungskurs4, leistungskurs5, leistungskurs6, leistungskurs7,
+                    ma0, ma1, ma2, ma3,
+                    de0, de1, de2, de3,
+                    ge0, ge1, ge2, ge3,
+                    km0, km1, km2, km3,
+                    pr0, pr1, pr2, pr3,
+                    in0, in1, in2, in3,
+                    fs0, fs1, fs2, fs3, fs4, fs5, fs6, fs7,
+                    nw0, nw1, nw2, nw3, nw4, nw5, nw6, nw7, nw8, nw9, nw10, nw11,
+                    gw0, gw1, gw2, gw3, gw4, gw5, gw6, gw7, gw8, gw9, gw10, gw11,
+                  ],
         mobileAgent: {
             Android() {
                 return navigator.userAgent.match(/Android/i)
@@ -27,35 +37,35 @@ new Vue({
     },
 
     methods: {
-        rechnen(umrechnung, punktzahl){
+        rechnen(umrechnung, leistungskurs00){
             let temp = []
             for (const i of umrechnung){
-                let pktzahl = Math.round((punktzahl*i/100)*100)/100
+                let pktzahl = Math.round((leistungskurs00*i/100)*100)/100
                 temp.push(pktzahl.toString().replace(".",",")) //Ersetzt das Dezimaltrennzeichen vom Punkt zum Komma
             }
             return temp
         },
-        addState(punktzahl){
+        addState(leistungskurs00){
             let stateObj = { id: "100" }; 
               
             window.history.pushState(stateObj, 
-                     "Punktzahl", "?punkte=" + punktzahl);
+                     "leistungskurs00", "?LF11=" + leistungskurs00);
         }
     },
 
     computed: {
         klausurNotenRechner(){
-            if (parseFloat(this.punktzahl.replace(",",".")) > 0 && typeof parseFloat(this.punktzahl) == "number"){
-                this.addState(this.punktzahl)
-                return this.rechnen(this.umrechnungKlausurNoten, parseFloat(this.punktzahl.replace(",", ".")))
+            if (parseFloat(this.leistungskurs00.replace(",",".")) > 0 && typeof parseFloat(this.leistungskurs00) == "number"){
+                this.addState(this.leistungskurs00)
+                return this.rechnen(this.umrechnungKlausurNoten, parseFloat(this.leistungskurs00.replace(",", ".")))
             } else {
                 return " ,".repeat(15).split(",")
             }
         },
         sonstNotenRechner(){
-            if (parseFloat(this.punktzahl.replace(",",".")) > 0 && typeof parseInt(this.punktzahl) == "number"){
-                this.addState(this.punktzahl)
-            return this.rechnen(this.umrechnungSonstNoten, parseFloat(this.punktzahl.replace(",", ".")))
+            if (parseFloat(this.leistungskurs00.replace(",",".")) > 0 && typeof parseInt(this.leistungskurs00) == "number"){
+                this.addState(this.leistungskurs00)
+            return this.rechnen(this.umrechnungSonstNoten, parseFloat(this.leistungskurs00.replace(",", ".")))
             } else {
                 return " ,".repeat(15).split(",")
             }
@@ -66,9 +76,9 @@ new Vue({
         let toc = document.querySelector(".toc")
 
         if (URLSearcher.has("punkte")){
-            this.punktzahl = URLSearcher.get("punkte")
+            this.leistungskurs00 = URLSearcher.get("punkte")
         } else {
-            this.punktzahl = ""
+            this.leistungskurs00 = ""
         }
 
         // Ueberprueft das Geraet, ob es ein Mobiles Endgeraet ist
