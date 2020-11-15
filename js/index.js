ab0410d5001af2b035d8e6a83f22b03d2b09d732
Adrian <adrian.hammermeister55@gmail.com>
2020-11-15 19:08:41 +0100

    index.html und index.js angepasst
    index.html in verpflichtende fächer und sonstige unterteilt (Block I)
    
    index.js: Berechnungen hinzugefügt

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 new Vue({
     el: "#app",
     data: {
-        umrechnungKlausurNoten : [95,90,85,80,75,70,65,60,55,50,45,40,33,27,20,0],
-        umrechnungSonstNoten : [98.67,97.33,96,90.67,85.33,80,73.33,66.67,60,55.33,46.67,40,33.33,26.67,20,0],
-        parentMessage: 'Parent',
-        semester: {leistungskurs0: "", leistungskurs1: "", leistungskurs2: "", leistungskurs3: "", leistungskurs4: "", leistungskurs5: "", leistungskurs6: "", leistungskurs7: "",
-                    ma0: "", ma1: "", ma2: "", ma3: "",
-                    de0: "", de1: "", de2: "", de3: "",
-                    ge0: "", ge1: "", ge2: "", ge3: "",
-                    km0: "", km1: "", km2: "", km3: "",
-                    pr0: "", pr1: "", pr2: "", pr3: "",
-                    in0: "", in1: "", in2: "", in3: "",
-                    fs0: "", fs1: "", fs2: "", fs3: "", fs4: "", fs5: "", fs6: "", fs7: "",
-                    nw0: "", nw1: "", nw2: "", nw3: "", nw4: "", nw5: "", nw6: "", nw7: "", nw8: "", nw9: "", nw10: "", nw11: "",
-                    gw0: "", gw1: "", gw2: "", gw3: "", gw4: "", gw5: "", gw6: "", gw7: "", gw8: "", gw9: "", gw10: "", gw11: "",
-                  },
-        pruefungsfaecher= {pf0: "", pf1: "", pf2: "", pf3: ""},
+        selectedlk1: "",
+        selectedlk2: "",
+        selectedgk3: "",
+        selectedgk4: "", 
+        selectedgk5: "",
+        blockI: 0,
+        blockII: 0,
+        onefs: 1,
+        warning: false,
+        nw: ["Chemie", "Biologie", "Physik"],
+        gw: ["Geschichte", "Geografie", "Philosophie", "Religion", "Sozialkunde", "Wirtschaft"],
+        fs: ["Englisch", "Französisch", "Spanisch", "Latein"],
+        blockIPflicht: 0, 
+        hjleft: 0,
+
         mobileAgent: {
             Android() {
                 return navigator.userAgent.match(/Android/i)
@@ -39,62 +39,76 @@ new Vue({
     },
 
     methods: {
-        rechnenblockI(semester){
-          for (const i of semester) {
-
-          }
-        },
-        rechnen(umrechnung, leistungskurs00){
-            let temp = []
-            for (const i of umrechnung){
-                let pktzahl = Math.round((leistungskurs00*i/100)*100)/100
-                temp.push(pktzahl.toString().replace(".",",")) //Ersetzt das Dezimaltrennzeichen vom Punkt zum Komma
+        changeLK(){
+            let lk1 = document.querySelector("#lk1")
+            let lk2 = document.querySelector("#lk2")
+            this.selectedlk1 = lk1.options[lk1.selectedIndex].value
+            if (lk2.options[lk2.selectedIndex].value != this.selectedlk1){
+                this.selectedlk2 = lk2.options[lk2.selectedIndex].value
+                this.warning = false
+                this.blockIPflicht = document.querySelectorAll(".blockI-pflicht").length
+                this.hjleft = 36-this.blockIPflicht
+            } else if (this.selectedlk1 == ""){
+                this.selectedlk2 = lk2.options[lk2.selectedIndex].value
+                this.warning = false
+                this.blockIPflicht = document.querySelectorAll(".blockI-pflicht").length
+                this.hjleft = 36-this.blockIPflicht
+            } else {
+                this.selectedlk2 = lk2.options[lk2.selectedIndex].value
+                this.warning = true
+                this.blockIPflicht = document.querySelectorAll(".blockI-pflicht").length
+                this.hjleft = 36-this.blockIPflicht
             }
-            return temp
+            
         },
-        addState(leistungskurs0){
-            let stateObj = { id: "100" }; 
-              
-            window.history.pushState(stateObj, 
-                     "leistungskurs0", "?LF11=" + leistungskurs0);
+        changeOneFs(event){
+            this.onefs = event.target.value
+            this.blockIPflicht = document.querySelectorAll(".blockI-pflicht").length
+            this.hjleft = 36-this.blockIPflicht
+        },
+        changePF(){
+            let gk3 = document.querySelector("#gk3")
+            let gk4 = document.querySelector("#gk4")
+            let gk5 = document.querySelector("#gk5")
+            this.selectedgk3 = gk3.options[gk3.selectedIndex].value
+            this.selectedgk4 = gk4.options[gk4.selectedIndex].value
+            this.selectedgk5 = gk5.options[gk5.selectedIndex].value
+            this.blockIPflicht = document.querySelectorAll(".blockI-pflicht").length
+            this.hjleft = 36-this.blockIPflicht
+        },
+        calculateBlock1(){
+            let lk = document.querySelectorAll(".markLK")
+            let gk = document.querySelectorAll(".markGK")
+            let sum = 0
+            for (element of lk){
+                if (parseInt(element.value) == NaN){
+                    sum += 0
+                    continue
+                } 
+                sum += 2*parseInt(element.value)
+                
+            }
+            for (element of gk){
+                if (parseInt(element.value) == NaN){
+                    sum += 0
+                    continue
+                } 
+                sum += parseInt(element.value)
+            }
+            this.blockI = sum
+            delete lk, gk, sum
+        },
+        calculateBlock2(){
+            let pf = document.querySelectorAll(".markPF")
+            let sum = 0
+            for (element of pf){
+                sum += 4*parseInt(element.value)
+            }
+            this.blockII = sum
         }
+
     },
 
-    computed: {
-        klausurNotenRechner(){
-            if (parseFloat(this.leistungskurs0.replace(",",".")) > 0 && typeof parseFloat(this.leistungskurs0) == "number"){
-                this.addState(this.leistungskurs0)
-                return this.rechnen(this.umrechnungKlausurNoten, parseFloat(this.leistungskurs00.replace(",", ".")))
-            } else {
-                return " ,".repeat(15).split(",")
-            }
-        },
-        sonstNotenRechner(){
-            if (parseFloat(this.leistungskurs0.replace(",",".")) > 0 && typeof parseInt(this.leistungskurs0) == "number"){
-                this.addState(this.leistungskurs0)
-            return this.rechnen(this.umrechnungSonstNoten, parseFloat(this.leistungskurs00.replace(",", ".")))
-            } else {
-                return " ,".repeat(15).split(",")
-            }
-        }
-    },
-    created(){
-        let URLSearcher = new URLSearchParams(window.location.search)
-        let toc = document.querySelector(".toc")
-
-        if (URLSearcher.has("punkte")){
-            this.leistungskurs0 = URLSearcher.get("punkte")
-        } else {
-            this.leistungskurs0 = ""
-        }
-
-        // Ueberprueft das Geraet, ob es ein Mobiles Endgeraet ist
-        if (this.mobileAgent.any()){
-            toc.style.display = 'block'
-        } else {
-            toc.style.display = 'none'
-        }
-    }
 
 
 })
